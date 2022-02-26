e78dc880694e2c2834a1335e6dae1f3d15125c24
Gris Ge <fge@redhat.com>
2022-02-26 08:41:39 +0800

    mzc: Add clap support
    Signed-off-by: Gris Ge <fge@redhat.com>

--- src/mzc.rs
+++ src/mzc.rs
@@ -1,12 +1,24 @@
+use clap::Parser;
 use mozim::{DhcpV4Client, DhcpV4Config, DhcpV4Lease};
 use nispor::{IfaceConf, IfaceState, IpAddrConf, IpConf, NetConf, NetState};
 
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = "DHCPv4 Client")]
+struct Args {
+    iface: String,
+}
+
 fn main() {
+    let args = Args::parse();
+
+    let iface_name = args.iface.as_str();
+
     let mut log_builder = env_logger::Builder::new();
     log_builder.filter(Some("mozim"), log::LevelFilter::Debug);
     log_builder.init();
 
-    let mut config = DhcpV4Config::new("eth1").unwrap();
+    let mut config = DhcpV4Config::new(iface_name).unwrap();
     config.set_host_name("Gris-Laptop");
     config.use_host_name_as_client_id();
     let mut cli = DhcpV4Client::new(config);
@@ -14,18 +26,18 @@ fn main() {
     let mut lease = cli.request(None).unwrap();
 
     println!("Got lease {:?}", lease);
-    apply_dhcp_ip("eth1", &lease);
+    apply_dhcp_ip_route(iface_name, &lease);
 
     loop {
         match cli.run(&lease) {
             Ok(l) => {
                 println!("new lease {:?}", l);
-                apply_dhcp_ip("eth1", &l);
+                apply_dhcp_ip_route(iface_name, &l);
                 lease = l;
             }
             Err(e) => {
                 println!("error {:?}", e);
-                purge_dhcp_ip("eth1");
+                purge_dhcp_ip_route(iface_name);
                 break;
             }
         }
@@ -42,7 +54,7 @@ fn new_net_conf_with_ip_conf(iface_name: &str, ip_conf: IpConf) -> NetConf {
     net_conf
 }
 
-fn apply_dhcp_ip(iface_name: &str, lease: &DhcpV4Lease) {
+fn apply_dhcp_ip_route(iface_name: &str, lease: &DhcpV4Lease) {
     let mut ip_addr_conf = IpAddrConf::default();
     ip_addr_conf.address = lease.yiaddr.to_string();
     ip_addr_conf.prefix_len = get_prefix_len(&lease.subnet_mask);
@@ -59,8 +71,8 @@ fn get_prefix_len(ip: &std::net::Ipv4Addr) -> u8 {
     u32::from_be_bytes(ip.octets()).count_ones() as u8
 }
 
-// Remove all dynamic IP
-fn purge_dhcp_ip(iface_name: &str) {
+// Remove all dynamic IP and dhcp routes of specified interface
+fn purge_dhcp_ip_route(iface_name: &str) {
     let state = NetState::retrieve().unwrap();
     if let Some(ip_info) =
         state.ifaces.get(iface_name).and_then(|i| i.ipv4.as_ref())
